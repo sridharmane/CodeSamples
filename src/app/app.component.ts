cedca66d4dba9adc37d1e4b5a6472e7b079931f9
Sridhar Mane <sridhars4s@gmail.com>
2017-03-28 00:23:22 -0400

    Added Fibonacci Series with For and Recursion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,55 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  fibPosition = 0;
+  fibSeriesWithFor :number[] = [];
+  performance = {
+    fibSeriesWithFor:0,
+    fibSeriesWithRecursion:0
+  };
+  values = {
+    fibNumberWithFor :0,
+    fibNumberWithRecursion:0
+  }
+  fibSeriesWithRecursion :any = [];
+
+  setFibPosition(event:any){
+    // console.log(event);
+    this.fibPosition = Math.abs(event.target.value);
+    console.trace();
+    this.generateFibSeriesWithFor();
+    this.generateFibSeriesWithRecursion();
+  }
+  generateFibSeriesWithFor(){
+    let t0 = performance.now();
+    let x=0, y=1, currentSum=0;
+    this.fibSeriesWithFor = [];
+    for(let i = 0; i< this.fibPosition; i++){
+      if(i < 1){
+        // this.fibSeriesWithFor.push(1);
+      }else{
+      currentSum = x+y;
+      // this.fibSeriesWithFor.push(currentSum);
+      x = y;
+      y = currentSum;
+      }
+    }
+    this.values.fibNumberWithFor= currentSum;
+
+    let t1 = performance.now();
+    this.performance.fibSeriesWithFor = t1-t0;
+  }
+  generateFibSeriesWithRecursion(){
+    let t0 = performance.now();
+    this.values.fibNumberWithRecursion = this.fibRecursion(this.fibPosition);
+    let t1 = performance.now();
+    this.performance.fibSeriesWithRecursion = t1-t0;
+  }
+  fibRecursion(position: number){
+    if(position <= 1){
+      return position;
+    }else{
+      return this.fibRecursion(position-1) + this.fibRecursion(position-2)
+    }
+  }
 }
